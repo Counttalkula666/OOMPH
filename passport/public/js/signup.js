c055fa060c78b734f43f39d92a5c9cfb3a5bf9d9
unknown <drewradley@gmail.com>
2018-10-10 12:11:11 -0700

    hope so!

---
+++ passport/public/js/signup.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+  // Getting references to our form and input
+  var signUpForm = $("form.signup");
+  var emailInput = $("input#email-input");
+  var passwordInput = $("input#password-input");
+  var firstName = $("input#FN-input");
+  var lastName = $("input#LN-input");
+  // When the signup button is clicked, we validate the email and password are not blank
+  signUpForm.on("submit", function(event) {
+    event.preventDefault();
+    var userData = {
+      email: emailInput.val().trim(),
+      password: passwordInput.val().trim(),
+      firstName: firstName.val().trim(),
+      lastName: lastName.val().trim()
+    };
+    console.log(userData)
+    if (!userData.email || !userData.password) {
+      return;
+    }
+    // If we have an email and password, run the signUpUser function
+    signUpUser(userData.email, userData.password, userData.firstName, userData.lastName);
+    emailInput.val("");
+    passwordInput.val("");
+    firstName.val("");
+    lastName.val("");
+  });
+
+  // Does a post to the signup route. If succesful, we are redirected to the members page
+  // Otherwise we log any errors
+  function signUpUser(email, password, firstName, lastName) {
+    $.post("/api/signup", {
+      email: email,
+      password: password,
+      firstName: firstName,
+      lastName: lastName
+    }).then(function(data) {
+      //window.location.replace(data);
+      // If there's an error, handle it by throwing up a boostrap alert
+    }).catch(handleLoginErr);
+  }
+
+  function handleLoginErr(err) {
+    $("#alert .msg").text(err.responseJSON);
+    $("#alert").fadeIn(500);
+  }
+});
