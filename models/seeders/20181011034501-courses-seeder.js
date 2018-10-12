488e7e6882325cdf21269c51c1a4992d1d3d49e5
Counttalkula666 <elenabelgum@gmail.com>
2018-10-12 10:12:21 -0700

    pushing changes to seeder model setup w correct fields etc

--- models/seeders/20181011034501-courses-seeder.js
+++ models/seeders/20181011034501-courses-seeder.js
@@ -16,36 +16,43 @@ module.exports = {
         allowNull: false,
         createdAt: new Date(),
         pdatedAT: new Date()
-      },{    
+      },
       courseName: { type: DataTypes.STRING,
         allowNull: false,
         createdAt: new Date(),
         updatedAT: new Date()
-      },{  
+      },
       courseDesc: { type: DataTypes.TEXT,
         allowNull: false,
         createdAt: new Date(),
         updatedAT: new Date()
-      },{  
+      },
+      termOffered: { type: DataTypes.STRING,
+        allowNull: false,
+        createdAt: new Date(),
+        updatedAT: new Date()
+      },
+      midProcExam: { type: DataTypes.STRING,
+        createdAt: new Date(),
+        updatedAT: new Date()
+      },         
+      finProcExam: { type: DataTypes.STRING,
+        createdAt: new Date(),
+        updatedAT: new Date()
+      },         
+      hpmReq:  { type: DataTypes.STRING,
+        createdAt: new Date(),
+        updatedAT: new Date()
+      },         
+      intReq: { type: DataTypes.STRING,
+        createdAt: new Date(),
+        updatedAT: new Date()
+      },         
       courseNum: { type: DataTypes.TEXT,
         allowNull: false,
         createdAt: new Date(),
         updatedAT: new Date()
-      },{              
-      courseNum: { type: DataTypes.TEXT,
-      allowNull: false,
-      createdAt: new Date(),
-      updatedAT: new Date()
-      },{  
-      course_enrollment_num: INT(6),
-      course_name: string(50),
-      course_description: text(500),
-      term_offered: string(40),
-      mid_proc_exam: string(50),
-      fin_proc_exam :string(50),
-      hpm_req: string(50),
-      int_req: string(50),
-      PRIMARY KEY (id) 
+      },        
       }]);
    
   },
